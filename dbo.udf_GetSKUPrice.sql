1bb12aea427f1de902327d0a1faf52110207c5e6
44East <antswert@gmail.com>
2023-04-08 11:24:15 +0300

    Add function query

---
+++ dbo.udf_GetSKUPrice.sql
@@ -0,0 +1,20 @@
+CREATE FUNCTION dbo.udf_GetSKUPrice
+    (@ID_SKU INT)
+RETURNS DECIMAL(18, 2)
+AS
+BEGIN
+    DECLARE @TotalValue DECIMAL(18, 2)
+    DECLARE @TotalQuantity DECIMAL(18, 2)
+    DECLARE @Price DECIMAL(18, 2)
+    
+    SELECT @TotalValue = SUM(Value), @TotalQuantity = SUM(Quantity)
+    FROM dbo.Basket
+    WHERE ID_SKU = @ID_SKU
+    
+    IF @TotalQuantity > 0
+        SET @Price = @TotalValue / @TotalQuantity
+    ELSE
+        SET @Price = 0
+    
+    RETURN @Price
+END
