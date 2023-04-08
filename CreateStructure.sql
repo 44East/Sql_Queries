68af3865afbcbd1c32adfcc4409c6973da9ab34e
44East <antswert@gmail.com>
2023-04-08 11:10:58 +0300

    Edit creation query

--- CreateStructure.sql
+++ CreateStructure.sql
@@ -1,27 +1,29 @@
 -- Создание таблицы SKU
 CREATE TABLE dbo.SKU (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    Code VARCHAR(50) UNIQUE,
-    Name VARCHAR(255)
-)
-
--- Создание вычисляемого поля Code
-ALTER TABLE dbo.SKU ADD Code AS 's' + CAST(ID AS VARCHAR(10))
+    Code AS 's' + CAST(ID AS VARCHAR(10)),
+    Name NVARCHAR(50) NOT NULL,
+    CONSTRAINT UQ_SKU_Code UNIQUE (Code)
+);
 
 -- Создание таблицы Family
 CREATE TABLE dbo.Family (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    SurName VARCHAR(255),
-    BudgetValue DECIMAL(18, 2)
-)
+    SurName NVARCHAR(50) NOT NULL,
+    BudgetValue DECIMAL(18,2) NULL
+);
 
 -- Создание таблицы Basket
 CREATE TABLE dbo.Basket (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    ID_SKU INT REFERENCES dbo.SKU(ID),
-    ID_Family INT REFERENCES dbo.Family(ID),
-    Quantity INT CHECK (Quantity >= 0),
-    Value DECIMAL(18, 2) CHECK (Value >= 0),
-    PurchaseDate DATETIME2 DEFAULT GETDATE(),
-    DiscountValue DECIMAL(18, 2) DEFAULT 0
-)
+    ID_SKU INT NOT NULL,
+    ID_Family INT NOT NULL,
+    Quantity DECIMAL(18,2) NOT NULL CHECK (Quantity >= 0),
+    Value DECIMAL(18,2) NOT NULL CHECK (Value >= 0),
+    PurchaseDate DATETIME2 NOT NULL DEFAULT(GETDATE()),
+    DiscountValue DECIMAL(18,2) NULL,
+    CONSTRAINT FK_Basket_SKU FOREIGN KEY (ID_SKU) REFERENCES dbo.SKU(ID),
+    CONSTRAINT FK_Basket_Family FOREIGN KEY (ID_Family) REFERENCES dbo.Family(ID),
+    CONSTRAINT CHK_Quantity CHECK (Quantity >= 0),
+    CONSTRAINT CHK_Value CHECK (Value >= 0)
+);
