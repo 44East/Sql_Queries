7f32f45dbd3f6c194744fb140c0aa71b4decb201
Denis Kazazaev <102745770+44East@users.noreply.github.com>
2023-04-09 11:22:26 +0300

    Update dbo.usp_MakeFamilyPurchase.sql

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -2,14 +2,14 @@ CREATE PROCEDURE dbo.usp_MakeFamilyPurchase
     @FamilySurName NVARCHAR(255)
 AS
 BEGIN
-    -- Проверяем, существует ли семья с такой фамилией
+    -- Check family with that surname for exists 
     IF NOT EXISTS (SELECT 1 FROM dbo.Family WHERE SurName = @FamilySurName)
     BEGIN
-        RAISERROR('Семьи с фамилией %s не существует.', 16, 1, @FamilySurName)
+        RAISERROR('Г‘ГҐГ¬ГјГЁ Г± ГґГ Г¬ГЁГ«ГЁГҐГ© %s Г­ГҐ Г±ГіГ№ГҐГ±ГІГўГіГҐГІ.', 16, 1, @FamilySurName)
         RETURN
     END
 
-    -- Обновляем значение поля BudgetValue у семьи
+    -- Refresh value from BudgetValue field at family
     UPDATE dbo.Family
     SET BudgetValue = BudgetValue - (SELECT SUM(Value) FROM dbo.Basket WHERE ID_Family = (SELECT ID FROM dbo.Family WHERE SurName = @FamilySurName))
     WHERE SurName = @FamilySurName
