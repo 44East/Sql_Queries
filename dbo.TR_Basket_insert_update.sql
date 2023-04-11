058941928a626b5c47d6a5163bd909ef2d73bf6a
Denis Kazazaev <102745770+44East@users.noreply.github.com>
2023-04-11 22:03:15 +0300

    Update dbo.TR_Basket_insert_update.sql

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -5,18 +5,18 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    -- calculating DiscountValue for each ID_SKU
+    WITH sku_counts AS (
+        SELECT ID_SKU, COUNT(*) AS Count
+        FROM inserted
+        GROUP BY ID_SKU
+    )
     UPDATE b
     SET DiscountValue = CASE 
-                          WHEN sku_counts.Count > 1 THEN b.Value * 0.05 
+                          WHEN c.Count > 1 THEN b.Value * 0.05 
                           ELSE 0 
                         END
     FROM dbo.Basket AS b
-    INNER JOIN (
-      SELECT ID_SKU, COUNT(*) AS Count
-      FROM inserted
-      GROUP BY ID_SKU
-    ) AS sku_counts ON b.ID_SKU = sku_counts.ID_SKU
+    INNER JOIN sku_counts AS c ON b.ID_SKU = c.ID_SKU
     INNER JOIN dbo.SKU AS s ON s.ID = b.ID_SKU;
 END;
 GO
